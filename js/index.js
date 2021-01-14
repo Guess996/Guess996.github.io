c6d8ca246b6a08751b2397c5458107fe0fac7bd1
Guess996 <61116196+Guess996@users.noreply.github.com>
2021-01-14 18:30:30 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 let show1=true
 function showaside(){
             if(show1){
-                // let topx=parseInt(document.documentElement.scrollTop||document.body.scrollTop)
-                // // console.log(topx);
-                // // document.querySelector(".asides").style.top = topx+"px"
                 document.querySelector(".asides").style.width="250px"
                 document.body.style.overflow="hidden"
                 show1=false
@@ -23,34 +20,6 @@ window.onload=()=>{
     let index=0
     let times=null
     let time1=null
-    // for(let i=0;i<lbt_li.length;i++){
-    //     console.log(i);
-    //     lbt_but[i].onclick=()=>{
-    //         index=i
-    //         for(let j=0;j<lbt_li.length;j++){
-    //             lbt_but[j].style.backgroundColor="rgba(255, 255, 255,.5)"
-    //             lbt_li[j].style.opacity="0"
-    //         }
-    //         lbt_but[i].style.backgroundColor="#29B6F6"
-    //         lbt_li[i].style.opacity="1"
-    //     }  
-    // }
-    // times=setInterval(function(){
-    //     index++
-    //     if(index>3){
-    //         index=0
-    //     }
-    //     for(let j=0;j<lbt_li.length;j++){
-    //         lbt_but[j].style.backgroundColor="rgba(255, 255, 255,.5)"
-    //         lbt_li[j].style.opacity="0"
-    //     }
-    //     lbt_but[index].style.backgroundColor="#29B6F6"
-    //     lbt_li[index].style.opacity="1"
-        
-    // },10000)
-    // let lbt_but=document.querySelectorAll('.lbt_but>button')
-    // console.log(lbt_but);
-    // console.log(lbt_li[0]);
     for(let i= 0;i < lbt_li.length;i++){
         lbt_but[i].onclick=()=>{
             index=i
@@ -90,13 +59,21 @@ window.onload=()=>{
         }else{
             window.location.assign("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&ch=&tn=baidu&bar=&wd="+wtf.value+"&fenlei=256&oq=&rsv_pq=d7bc3b9c000066c5&rsv_t=bbf75sy%2F%2BdaG7oyR9XQSOqb9tenrNZBTUe1FrkfY%2Futf7zWy0xH9eMOa9IE&rqlang=cn&rsv_enter=0&rsv_btype=i&rsv_dl=ib&inputT=2304")
         }
-        // console.log(wtf.value);
     }
-    //++
-    // document.querySelector('.hms').innerHTML=days.toLocaleTimeString()
-    //     document.querySelector('.ymd').innerHTML=days.toLocaleDateString()
-    //     document.querySelector('.xq').innerHTML="星期"+days.getDay()
-    // console.log(days.toLocaleDateString(),days.toLocaleTimeString());
+    // let xx=false
+    // let lenghtss=document.querySelector(".website").children
+    // document.querySelector('.titles').addEventListener('click',()=>{
+    //     xx = !xx
+    //     if(xx){
+    //         document.querySelector('.du').style.transform="rotate(90deg)"
+    //         document.querySelector('aside .xx ul').style.height=lenghtss.length*40+"px"
+    //     }else{
+    //         document.querySelector('.du').style.transform="rotate(0)"
+    //         document.querySelector('aside .xx ul').style.height=""
+            
+    //     }
+        
+    // })
 // 获取当前日期时间
 // var myDate = new Date();
 // myDate.toLocaleDateString();                //获取当前日期
